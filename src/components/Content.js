c056e2af5052082b19fe190089d090cf1f73483d
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-08-23 00:54:13 +0300

    added vote icon, edited content page

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Star from "../assets/icons/Star";
 
 const Content = () => {
+  const [movie, setMovie] = useState([]);
+  const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/popular?api_key=ccb0a8566b23ab43471cda53fed3d9e7&language=en-US&page=1"
+      )
+      .then((response) => setMovie(response.data.results));
+  }, []);
+
   return (
     <section className="section">
-      <div className="section__container"></div>
+      <div className="section__container">
+        {movie.map((card) => (
+          <div key={card.id} className="section__card">
+            <div className="section__card__img">
+              <img src={IMG_API + card.poster_path} alt={card.poster_path} />
+            </div>
+            <div className="section__card__title">
+              <h2>{card.title}</h2>
+            </div>
+            <div className="section__card__vote">
+              <Star />
+              <p>{card.vote_average}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Content from "../components/Content";
 import Input from "../components/Input";
 
 const Home = () => {
   return (
     <div className="home">
       <Input title="Movies" />
+      <Content />
     </div>
   );
 };
