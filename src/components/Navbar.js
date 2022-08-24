c387fb5daf4476a1b75238ef02515c98528a5fb4
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-08-24 14:57:46 +0300

    responsive fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/images/logo.png";
+import { FaBars, FaTimes } from "react-icons/fa";
+
 const Navbar = () => {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
   const links = [
     {
       id: 1,
@@ -21,20 +29,27 @@ const Navbar = () => {
 
   return (
     <nav className="nav">
-      <div className="nav__container">
+      <div ref={navRef} className="nav__container">
         <div className="nav__logo">
           <Link to="/">
-            <img src={Logo} alt="" />
+            <img src={Logo} alt="" onClick={showNavbar} />
           </Link>
         </div>
         <ul className="nav__list">
           {links.map((item) => (
-            <li key={item.id} className="nav__link">
+            <li key={item.id} className="nav__link" onClick={showNavbar}>
               <Link to="/">{item.name}</Link>
             </li>
           ))}
         </ul>
+        <button className="nav__btn nav__closebtn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
       </div>
+
+      <button className="nav__btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
     </nav>
   );
 };
